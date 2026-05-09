d33f3f3099eaf9b56e350d5f632a72f350551e3f
BayiHY <95440881@qq.com>
2026-05-09 15:33:22 +0800

    feat: 同步到GitHub，实现三平台同步

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 同时推送到 Gitee 和 GitCode
+# 同时推送到 Gitee、GitCode 和 GitHub
 
 # 提交信息
 if [ -z "$1" ]; then
@@ -14,11 +14,14 @@ git add -A
 # 提交
 git commit -m "$1"
 
-# 推送到两个平台
+# 推送到三个平台
 echo "推送到 Gitee..."
 git push origin master
 
 echo "推送到 GitCode..."
 git push gitcode master
+
+echo "推送到 GitHub..."
+git push github master
 
 echo "✅ 同步完成！"
