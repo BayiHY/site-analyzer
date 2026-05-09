27224525b3abcb1da54f4ca8125a052d75a892ec
BayiHY <95440881@qq.com>
2026-05-09 14:01:27 +0800

    添加双平台推送脚本

---
+++ push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# 同时推送到 Gitee 和 GitCode
+
+# 提交信息
+if [ -z "$1" ]; then
+    echo "请提供提交信息"
+    echo "用法: ./push.sh '提交信息'"
+    exit 1
+fi
+
+# 添加所有更改
+git add -A
+
+# 提交
+git commit -m "$1"
+
+# 推送到两个平台
+echo "推送到 Gitee..."
+git push origin master
+
+echo "推送到 GitCode..."
+git push gitcode master
+
+echo "✅ 同步完成！"
